b228ae648b196a0b8f84e1da11ed15cc718973f1
Sandor <131536526+SandorZsomborSzabo@users.noreply.github.com>
2025-06-17 20:06:26 +0100

    Final Commit, Working Logic Function for conversion

--- currency_converter.py
+++ currency_converter.py
@@ -1,5 +1,36 @@
 import tkinter as tk
 
+
+"""
+Logic for conversion between each currency
+"""
+def conversion():
+    if opt1.get() == "EUR":
+        if opt2.get() == "EUR":
+            return float(textbox.get())
+        elif opt2.get() == "USD":
+            return float(textbox.get()) * 1.14
+        elif opt2.get() == "GBP":
+            return float(textbox.get()) * 0.85
+
+    elif opt1.get() == "USD":
+        if opt2.get() == "USD":
+            return float(textbox.get())
+        elif opt2.get() == "EUR":
+            return float(textbox.get()) * 0.87
+        elif opt2.get() == "GBP":
+            return float(textbox.get()) * 0.74
+
+    elif opt1.get() == "GBP":
+        if opt2.get() == "GBP":
+            return float(textbox.get())
+        elif opt2.get() == "USD":
+            return float(textbox.get()) * 1.34
+        elif opt2.get() == "EUR":
+            return float(textbox.get()) * 1.17
+
+    return None
+
 window = tk.Tk()
 
 window.geometry("350x450")
@@ -21,12 +52,16 @@ opt1 = tk.StringVar(value="EUR")
 opt2 = tk.StringVar(value="EUR")
 
 # Dropdown menu, from
-tk.OptionMenu(window, opt1, *currency_from).place(x=50, y=100, height=40, width=100)
+tk.OptionMenu(window, opt1, *currency_from).place(x=50, y=120, height=40, width=100)
+from_label = tk.Label(window, text="From", font=("Arial", 12)).place(x=70, y=100)
 
 # Dropdown menu, to
-tk.OptionMenu(window, opt2, *currency_to).place(x=200, y=100, height=40, width=100)
+tk.OptionMenu(window, opt2, *currency_to).place(x=200, y=120, height=40, width=100)
+to_label = tk.Label(window, text="To", font=("Arial", 12)).place(x=230, y=100)
 
-convert = tk.Button(window, text="Convert").place(x=50, y=150, height=40, width=250)
+def print_result():
+    result=tk.Label(window, text=f"Result is {conversion()}", bd=10, relief="solid").place(x=50, y=240, height=40, width=250)
 
+convert = tk.Button(window, text="Convert", command=print_result).place(x=50, y=170, height=40, width=250)
 
 window.mainloop()
